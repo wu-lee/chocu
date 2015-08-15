8361021542e21562def2cb2fbaee4bb977946ef2
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2015-08-15 21:48:24 +0100

    render/_ddoc/templates/edit.html - add nominal live editing

--- render/_ddoc/js/chocu.js
+++ render/_ddoc/js/chocu.js
@@ -13,3 +13,26 @@ angular.module('Chocu')
     });
 
 
+angular.module('Chocu')
+    .directive('chokuToHtml', function($window) {
+        return {
+            scope: true,
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                // Get the expression to watch from the choku-to-html attr
+                var expr = attrs.chokuToHtml;
+                scope.$watch(expr, function(newval, oldval) {
+                    // On change, get the markdown property's value,
+                    // if any.  It should contain markdown text.
+                    var markdown =
+                        newval &&
+                        newval.hasOwnProperty('markdown') &&
+                        newval.markdown != null ?
+                        newval.markdown : "";
+
+                    // Convert it to html and insert into the element.
+                    element.html($window.Markdown.toHTML(markdown));
+                }, true);
+            },
+        };
+    });
